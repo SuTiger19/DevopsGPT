4239aea6ac5faa80685d2512c8710fcd3cd1c6fd
Sudeep Saurabh <sudeepsaurabhss@gmail.com>
2025-06-20 22:36:50 -0400

    Update some step to tell prompte what to do create github action for terraform part3

--- local_llm/terraform_github.py
+++ local_llm/terraform_github.py
@@ -7,10 +7,18 @@ Terraform directory {terraform_dir}
 Module type: {module_type} (local, remote)  --- Is module we are using create locally or are you using remote module like private module or public module
 Backend config: {yes_or_no} -- Like AWS S3  ( Use state locking DDB , but S3 has locking now feature)
 Steps to include:
+    Use tflint: {yes_or_no}
+    Use tfsec: {yes_or_no}
     Format: {yes_or_no}
     Init: {yes_or_no}
     Validate: {yes_or_no}
     Plan: {yes_or_no} -- but as well terraform plan -output=file
+
+Save plan file: {yes_or_no}
+Output plan as PR comment: {yes_or_no}
+Upload SARIF: {yes_or_no}
+Upload artifacts/logs: {yes_or_no}
+
 
 '''
 
